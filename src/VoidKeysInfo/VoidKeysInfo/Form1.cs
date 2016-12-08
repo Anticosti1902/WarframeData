a3e43831c4fe7c24c39193e311ecc1b8772cbfc1
alexbarto <el.portug@gmail.com>
2016-12-08 00:00:57 -0500

    Creation of a new Pane for the programm, added .txt file for the data.

--- src/VoidKeysInfo/VoidKeysInfo/Form1.cs
+++ src/VoidKeysInfo/VoidKeysInfo/Form1.cs
@@ -10,11 +10,31 @@ using System.Windows.Forms;
 
 namespace VoidKeysInfo
 {
-    public partial class Form1 : Form
+    public partial class MainPanel : Form
     {
-        public Form1()
+        public MainPanel()
         {
             InitializeComponent();
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
